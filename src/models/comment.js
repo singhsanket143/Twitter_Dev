bed7f9d92753ab64460b2ba8430ecdd2848215c7
Sanket Singh <singhsanket143@gmail.com>
2023-01-31 18:45:41 +0530

    Updated comment model with refpath

--- src/models/comment.js
+++ src/models/comment.js
@@ -5,9 +5,21 @@ const commentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    userEmail: {
-        type: String
+    userId: {
+        ype: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    }
 }, {timestamps: true});
 
 const Comment = mongoose.model('Comment', commentSchema);
